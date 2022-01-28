e5aedfac388574016f3583dc656f4af96edfde84
zarooba01 <zawistowskihubert01@gmail.com>
2022-01-28 20:50:13 +0100

    website docker added init

--- website/index.php
+++ website/index.php
@@ -9,6 +9,7 @@ $menu="
 <li class='lnav'><a href=?node=papers>Papers</a>
 <li class='lnav'><a target=_blank href=https://github.com/aamks/aamks>Github</a>
 <li class='lnav'><a href=?node=authors>Authors</a>
+<li class='lnav'><a href=?node=download>Get AAMKS on your system</a>
 
 </ul>
 ";
@@ -103,6 +104,140 @@ Contributors
 
 ");
 /*}}}*/
+/*}}}*/
+$nodes['download']=array("Get AAMKS on your system", /*{{{*/
+'
+<div class="content" style="margin-left: 5%; margin-bottom:20px"> 
+
+	<article>
+	<header>
+	<div>
+	<h1 class="h2"> Get AAMKS on your system </h1>
+	</div>
+	</header>
+	<content>
+
+			<div class="step">	
+				<div class="h3">
+					1. Install Docker
+				</div>
+				<div class="info">
+						<p>Below instruction how to install docker on Ubuntu 20.04. If some issues appear, check <a href="https://docs.docker.com/engine/install/ubuntu/">here</a>. </p>
+						<p>How to install docker on different platforms,@ check <a href="https://docs.docker.com/engine/install/">here</a>. </p>
+						
+						<p>Open terminal and execute commands: </p>
+						<p style="font-size:small;"> <i>(always copy the text after $)</i> </p>
+						<p class="code"> $ sudo apt-get update </p>
+						<p class="code"> $ 	sudo apt-get install \
+							apt-transport-https \
+							ca-certificates \
+							curl \
+							gnupg \
+							lsb-release </p>
+						<p class="code"> $ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg </p>
+ 						<p class="code"> $ sudo apt-get install docker-ce docker-ce-cli containerd.io </p>
+ 						<p> <b>Check</b> that docker works:</p>
+ 						<p class="code"> $ docker</p>
+ 						<p style="font-size:small;"> <i>(<b>Check</b> is only to test process, its not necessery to execute)</i> </p>	
+ 						<img src="images/docker_test.png" width=740px>
+
+ 				</div>
+
+			</div>
+
+			<div class="step">	
+				<div class="h3">
+					2. Download image 
+
+				</div>
+				<div class="info">
+						<p> Download aamks image from dockerhub</p>
+						<p class="code"> $ docker pull zarooba/aamksinstall:working</p>
+						<p> <b>Check</b> that you have proper image downloaded:</p>
+						<p class="code"> $ docker image ls</p> 
+						<img src="images/image_ls.png" width=740px>
+
+
+ 				</div>
+
+			</div>
+
+			<div class="step">	
+				<div class="h3">
+					3. Create network 
+				</div>
+				<div class="info">
+						<p>Create network on which aamks will serve data, if you dont want to work on localhost</p>
+						<p class="code"> $ docker network create --subnet=192.168.0.0/16 net_aamks</p>
+						<p> <b>Check</b> that the network exists: </p>
+ 						<p class="code"> docker network ls</p>
+ 						<img src="images/networks.png" width=740px>
+
+
+ 				</div>
+
+			</div>
+
+			<div class="step">	
+				<div class="h3">
+					4. Run container
+				</div>
+				<div class="info">
+						<p> Run container based on aamks image </p>
+						<p class="code"> $ docker run -it --network=net_aamks zarooba/aamksinstall:working </p>
+ 						<p> Running container is shown in last line: root@------------:/usr/local#. The rest of commands should be executed in container.</p>
+						<img src="images/docker_run.png" width=740px>
+ 				</div>
+
+			</div>
+
+			<div class="step">	
+				<div class="h3">
+					5. Restart apache and postgres
+				</div>
+				<div class="info">
+						<p> Apache2 and Postgresql have to be restarted to work properly</p>
+						<p> Restart these services in working container (root@------------:/usr/local#) </p>
+						<p class="code"> $ service apache2 restart</p>
+ 						<p class="code"> $ service postgresql restart</p>
+ 						<img src="images/restart.png" width=740px>
+ 						<p></p>
+ 						<p> During restarting apache2 you will see message: </p>
+ 						<p class="code"> <i>AH00558: apache2: Could not reliably determine the servers fully qualified domain name, using <b>170.0.0.2.</b> Set the 'ServerName' directive globally to suppress this message </i></p>
+ 						<p> IP that appears in message (in this case <b>170.0.0.2</b>) is the IP that has to be written in your browser (google chrome). By this IP your browser will connect with AAMKS.
+
+ 
+
+ 				</div>
+
+			</div>
+
+			<div class="step">	
+				<div class="h3">
+					6. Work on your project!
+				</div>
+				<div class="info"">
+						<p> Open google chrome on your computer. If you dont have it, download <a href ="https://www.google.com/intl/pl/chrome/">here</a> </p>
+ 						<p> Run <b>170.0.0.2/aamks</b> (in this case - IP from step 5.) in google chrome. You should see this:</p>
+ 						<img src="images/link.png" width=370px>
+ 						<p> AAMKS home site should be shown. Click register (under the login form) and fill register form.</p>
+						<img src="images/home_site.png" width=370px>
+						<p class="h4 mt-2"> Now you can work on your project!</p>
+						
+
+ 				</div>
+
+			</div>
+
+		</content>
+
+		</div>
+
+	</article>
+
+	</div>
+
+');
 
 #Engine/*{{{*/
 if(!isset($_GET['node'])){
